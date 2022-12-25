1f31e3950bbcc06aa3edcd363b696f62c8cc6df3
Iordanis Paschalidis <iordanispaschalidis@Iordaniss-Mac-mini.local>
2022-12-25 10:36:31 +0000

    adding tests

--- client_test.go
+++ client_test.go
@@ -1,3 +1,48 @@
 package promtengineclient_test
 
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	promtengineclient "github.com/BinaryLogos/promt-engine-client"
+)
+
 func TestNewClient(t *testing.T) {
+	type testCases struct {
+		name       string
+		domain     string
+		httpClient *http.Client
+		err        error
+	}
+
+	tc := []testCases{
+		{
+			name:       `empty_domain`,
+			domain:     ``,
+			httpClient: &http.Client{},
+			err:        fmt.Errorf(`error_no_domain`),
+		},
+		{
+			name:       `empty_httpClient`,
+			domain:     `https://www.google.com`,
+			httpClient: nil,
+			err:        fmt.Errorf(`error_no_httpClient`),
+		},
+		{
+			name:       `valid_client`,
+			domain:     `https://www.google.com`,
+			httpClient: &http.Client{},
+			err:        nil,
+		},
+	}
+
+	for _, tc := range tc {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := promtengineclient.NewClient(tc.domain, tc.httpClient)
+			assert.Equal(t, tc.err, err)
+		})
+	}
+}
